b114d85daf161d2b4058fedc17f01cf643c567ae
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2026-07-08 21:50:44 +0300

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+migrate = Migrate()
 
 login_manager = LoginManager()
 
 login_manager.login_view = "main.login"
+
+login_manager.login_message = "Please login first."
+
+login_manager.login_message_category = "warning"
