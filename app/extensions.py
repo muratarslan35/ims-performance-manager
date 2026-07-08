0e7162920b3a3bcfd36650b8c0224ffee2b8424b
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2026-07-08 22:45:02 +0300

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -8,8 +8,12 @@ migrate = Migrate()
 
 login_manager = LoginManager()
 
-login_manager.login_view = "main.login"
+login_manager.login_view = "auth.login"
 
-login_manager.login_message = "Please login first."
+login_manager.login_message = "Lütfen giriş yapınız."
 
 login_manager.login_message_category = "warning"
+
+login_manager.session_protection = "strong"
+
+login_manager.refresh_view = "auth.login"
