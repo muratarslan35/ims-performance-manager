7b523dbac383d18a13e5dca00987d5918fb1d4a3
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2026-07-09 00:20:38 +0300

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,6 +1,50 @@
-"""
-Dashboard Module
+from flask import Blueprint
+from flask import render_template
 
-Sprint 2 içerisinde
-geliştirilecektir.
-"""
+from flask_login import login_required
+
+from app.models import (
+    Product,
+    Representative,
+    IMSUpload,
+    Target
+)
+
+dashboard_bp = Blueprint(
+    "dashboard",
+    __name__,
+    url_prefix="/dashboard"
+)
+
+
+@dashboard_bp.route("/")
+@login_required
+def index():
+
+    total_products = Product.query.filter_by(
+        is_active=True
+    ).count()
+
+    total_representatives = Representative.query.filter_by(
+        active=True
+    ).count()
+
+    total_targets = Target.query.count()
+
+    last_upload = IMSUpload.query.order_by(
+        IMSUpload.uploaded_at.desc()
+    ).first()
+
+    return render_template(
+
+        "dashboard.html",
+
+        total_products=total_products,
+
+        total_representatives=total_representatives,
+
+        total_targets=total_targets,
+
+        last_upload=last_upload
+
+    )
