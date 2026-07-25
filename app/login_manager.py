ae3c496b169b5b6db91e88129434ec511d59800e
Murat arslan <102037882+muratarslan35@users.noreply.github.com>
2026-07-25 20:26:20 +0300

    Update login_manager.py

--- app/login_manager.py
+++ app/login_manager.py
@@ -1,19 +1,14 @@
-from app.extensions import db
-from app.extensions import login_manager
-
+from app.extensions import db, login_manager
 from app.models import User
 
 
 @login_manager.user_loader
 def load_user(user_id):
-
     try:
-
-        return db.session.get(
-            User,
-            int(user_id)
-        )
-
+        return db.session.get(User, int(user_id))
     except (TypeError, ValueError):
-
         return None
+
+
+def init_app(app):
+    login_manager.init_app(app)
