4e0ee62dbfe0e154140688a94729198cc83866bd
Murat arslan <102037882+muratarslan35@users.noreply.github.com>
2026-08-26 22:09:50 +0300

    Install row-semantic aggregate identity

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,7 @@ from app.services.representative_resolver import install_representative_resolver
 from app.services.semantic_import_discovery import install_semantic_import_discovery
 from app.services.dynamic_import_contract import install_dynamic_import_contract
 from app.services.dynamic_import_refinement import install_dynamic_import_refinement
+from app.services.aggregate_identity_refinement import install_aggregate_identity_refinement
 from app.services.workbook_preflight import install_workbook_preflight
 from app.services.official_brick_spread_atomic import install_official_brick_spread_atomic
 from app.services.derived_master_verification import install_derived_verification_gate
@@ -172,6 +173,7 @@ def create_app(config_object=Config):
     install_semantic_import_discovery()
     install_dynamic_import_contract()
     install_dynamic_import_refinement()
+    install_aggregate_identity_refinement()
     install_workbook_preflight()
     install_official_brick_spread_atomic()
     install_derived_verification_gate()
